d772095749f887e79a8161ec69845d28285111f7
Hasith Kenula <keyhasith800@gmail.com>
2026-03-20 15:49:14 +0530

    update server.js-accommodation/upload routes

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ import dotenv from "dotenv";
 import connectDB from "./src/config/mongodb.js";
 import app from "./src/index.js";  // Import 'app' from index.js
 
+import accommodationRoutes from './routes/accommodationRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+
 dotenv.config();  // Load environment variables from .env
 
 // Connect to the database
 connectDB();
+
+// ==================== ROUTES ====================
+app.use('/api/accommodations', accommodationRoutes);
+app.use('/api/upload', uploadRoutes);
+
+
+// ==================== START SERVER ====================
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 3000;
 
